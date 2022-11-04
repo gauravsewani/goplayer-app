5edb3c55619da00882ca33ec08f455b659345d34
gauravsewani <lavishgps@gmail.com>
2022-11-04 18:26:45 +0530

    fetching api

---
+++ src/api.js
@@ -0,0 +1,41 @@
+const api_key = "6c0ab43c5dba492fa95e4448cd97233e";
+
+// Base Url
+const base_url = "https://api.rawg.io/api/";
+
+//getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+// console.log(getCurrentMonth());
+
+//getting the day
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+// console.log();
+
+//popular games
+const popular_games = `games?key=${api_key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
+
+console.log(popularGamesUrl());
